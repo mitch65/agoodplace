67d5a6bad3d0bec4780c2c2efac52124654fefb6
mitch65 <59338148+mitch65@users.noreply.github.com>
2021-01-27 11:21:11 +0100

    Update wpp.tsx

--- wpp.tsx
+++ wpp.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="wpp" tilewidth="128" tileheight="64" tilecount="8" columns="2">
- <image source="../../../../Downloads/wpp.png" width="256" height="256"/>
+ <image source="wpp.png" width="256" height="256"/>
 </tileset>
